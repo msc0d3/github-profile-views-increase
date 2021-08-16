3bd7c22092a7d9c30ad78726887b2a0285c5d7f7
Nguyen Dac Tai <44217992+msc0d3@users.noreply.github.com>
2021-08-16 11:47:33 +0700

    Add files via upload

---
+++ Logger.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubProfileViewsIncrement
+{
+    public static class Logger
+    {
+        /// <summary>
+        /// log writer
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="type"></param>
+        public static void Write(string message, TypeMessage type)
+        {
+            switch (type)
+            {
+                case TypeMessage.Debug:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"{DateTime.Now} [Debug] {message}");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case TypeMessage.Done:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"{DateTime.Now} [+] : {message}");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case TypeMessage.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"{DateTime.Now} [!] : {message}");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case TypeMessage.Info:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"{DateTime.Now} [-] : {message}");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+            }
+        }
+    }
+    /// <summary>
+    /// log types
+    /// </summary>
+    public enum TypeMessage
+    {
+        Error,
+        Done,
+        Debug,
+        Info,
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+namespace GithubProfileViewsIncrement
+{
+    class Program
+    {
+        /// <summary>
+        /// main program
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.Title = "Github Profile Views Increase";
+            Console.Write("Insert Link : ");
+            string link = Console.ReadLine();
+            while (true)
+            {
+                Task.Delay(TimeSpan.FromSeconds(new Random().Next(1,10))).Wait();
+                RequestToProfileLink(link).Wait();
+            }
+        }
+        /// <summary>
+        /// send request async
+        /// </summary>
+        /// <param name="link"></param>
+        /// <returns></returns>
+        public static async Task RequestToProfileLink(string link)
+        {
+            Logger.Write("Sending Request...", TypeMessage.Info);
+            HttpClient http = new HttpClient();
+            http.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)");
+            try
+            {
+                string response = http.GetAsync(link).Result.Content.ReadAsStringAsync().Result;
+                var matches = Regex.Matches(response, "fill-opacity=\".3\">(.*?)</text>"); //get count view from raw response
+                string countview = matches[1].Groups[1].Value;
+                Logger.Write($"Success ! : Count Views ---> {countview}", TypeMessage.Done);
+                response = null;
+                matches = null;
+            }
+            catch(Exception ex)
+            {
+                Logger.Write("Error : " + ex.Message, TypeMessage.Error);
+            }
+            finally
+            {
+                http.Dispose();
+            }
+        }
+    }
+}
